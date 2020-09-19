2705136360f17a344bf4daff7bd69ed76b089230
Joseph Morales <jrales@mit.edu>
2020-09-19 18:50:18 -0400

    Made constants.py, filled arbitrarily

---
+++ constants.py
@@ -0,0 +1,7 @@
+EXERCISES = ["Push Ups", "Lunges", "Crunches", "Curls", "Downward Dog", "Jog", "HIIT"]
+AEROBIC_EXERCISES = ["HIIT", "Jog", "Lunges"]
+UPPER_BODY_EXERCISES = ["Push Ups", "Curls"]
+
+MODIFIERS = [1, 2, 3, 4, 5] #Totally Arbitrary for now
+
+
