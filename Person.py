2900651306954d61d6733f266799869e6575ab0a
Joseph Morales <jrales@mit.edu>
2020-09-19 22:43:12 -0400

    Added more methods to Person.py

--- Person.py
+++ Person.py
@@ -14,17 +14,26 @@ class Person:
     def getCurrentExercise(self):
         return self.preferredExercises[self.currentExerciseIndex]
 
+    def updateLocation(self, loc=None):
+        if loc: self.currentLocation = loc               
+ 
+        watch = self.watch
+        ###do fancy watch calls###
+        self.currentLocation = watch.getLocation
+
     def checkLocation(self, currentLocation):
         if currentLocation in self.workoutLocations: return True
         return False
 
-    def checkActive(self):
+    def checkActive(self, ext=False):
+        if external: return external
+
         watch = self.watch
         ###do fancy watch calls###
         if watch.getActive(): return True
         return False
 
     def checkSendNotification(self, currentLocation):
-        if checkLocation() and checkActive(): return True
+        if checkLocation() and checkActive(ext=True): return True
         else: False
         
