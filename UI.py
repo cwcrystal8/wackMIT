6d661222cb8e1881a36fe16991656fc61f77e278
Joseph Morales <jrales@mit.edu>
2020-09-19 22:26:20 -0400

    Skeleton of UI code

---
+++ UI.py
@@ -0,0 +1,56 @@
+import [insert generic GUI library here]
+
+
+
+def makeWatch():
+    #Code for Watch image
+    pass
+
+def makeActivityButton():
+    #Simple button to simulate activity
+    pass
+
+def makeLocationDropDown():
+    #Simple dropdown for location
+    pass
+
+def makeNotificationWindow():
+    #Code for notif in watch
+    pass
+
+def updateNotifWindowMessage():
+    #Code to make a new message
+    pass
+
+def makeNotifButtons():
+    #Code for making the y/next/n buttons
+    pass
+
+def updateNotifButtons():
+    #Code to remove buttons
+    pass
+
+def pressYes():
+    #code for pressing yes
+    pass
+
+def pressNext():
+    #code for pressing next
+    pass
+
+def pressNo():
+    #code for pressing no
+    pass
+
+def setUp():
+    #Code for making the static UI
+    makeWatch()
+    makeActivityButton()
+    makeLocationDropDown()
+
+def sendExerciseNotif():
+    #Code when sending notification
+    makeNotificationWindow()
+    updateNotifWindowMessage()
+    makeNotifButtons()
+
